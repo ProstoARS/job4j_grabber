20a049c499cf4bca09517405f7cf59f8d74ee6ec
ProstoARS <arsudakov@gmail.com>
2022-02-03 11:41:42 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,15 +8,21 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element parent = td.parent();
-            System.out.println(parent.child(1).child(0).attr("href"));
-            System.out.println(parent.child(1).child(0).text());
-            SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-            System.out.println(parser.parse(parent.child(5).text()).toString());
-            System.out.println(parent.child(5).text());
+        int count = 0;
+        for (int i = 1; i <= 5; i++) {
+            String page = String.valueOf(i);
+            String url = String.format("https://www.sql.ru/forum/job-offers/%s", page);
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                System.out.println("------" + count++);
+                Element parent = td.parent();
+                System.out.println(parent.child(1).child(0).attr("href"));
+                System.out.println(parent.child(1).child(0).text());
+                SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+                System.out.println(parser.parse(parent.child(5).text()).toString());
+                System.out.println(parent.child(5).text());
+            }
         }
     }
 }
