e1743f9e26919dee4e60797f0ebba95911b35b9f
ProstoARS <arsudakov@gmail.com>
2022-02-07 20:32:29 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/Main.java
@@ -0,0 +1,33 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.SqlRuParse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("grabber.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Store psqlStore = new PsqlStore(config);
+            SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser(), 5);
+            List<Post> listFromHtml = sqlRuParse.list("https://www.sql.ru/forum/job-offers");
+            for (Post p : listFromHtml) {
+                psqlStore.save(p);
+            }
+            List<Post> listFromSql = psqlStore.getAll();
+            for (Post p : listFromSql) {
+                System.out.println(p);
+            }
+            System.out.println("---------------");
+            System.out.println(psqlStore.findById(3));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,87 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.SqlRuParse;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private final Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            String url = cfg.getProperty("jdbc.url");
+            String user = cfg.getProperty("jdbc.username");
+            String password = cfg.getProperty("jdbc.password");
+            cnn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement ps = cnn.prepareStatement("INSERT INTO post ("
+                + "name, text, link, created) VALUES (?,?,?,?)", Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getLink());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> list = new ArrayList<>();
+        try (PreparedStatement ps = cnn.prepareStatement("SELECT * FROM post")) {
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                list.add(createPost(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement ps = cnn.prepareStatement("SELECT * FROM post WHERE id = ?")) {
+            ps.setInt(1, id);
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) {
+                post = createPost(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    private Post createPost(ResultSet resultSet) throws SQLException {
+        return new Post(resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("text"),
+                resultSet.getTimestamp("created").toLocalDateTime());
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+}
