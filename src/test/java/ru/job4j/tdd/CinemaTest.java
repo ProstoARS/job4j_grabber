a28847da0b85b9c67d617b4e8fb8d057bf354dd1
ProstoARS <arsudakov@gmail.com>
2022-04-25 19:17:36 +0300

    2. Что такое TDD? [#4918 #293254]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tdd;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,30 +11,64 @@ import java.util.Calendar;
 import java.util.List;
 
 public class CinemaTest {
-//
-//    @Test
-//    public void whenBuy() {
-//        Account account = new AccountCinema();
-//        Cinema cinema = new Cinema3D();
-//        Calendar date = Calendar.getInstance();
-//        date.set(2020, 10, 10, 23, 00);
-//        Ticket ticket = cinema.buy(account, 1, 1, date);
-//        assertThat(ticket, is(new Ticket3D()));
-//    }
-//
-//    @Test
-//    public void whenFind() {
-//        Cinema cinema = new Cinema3D();
-//        cinema.add(new Session3D());
-//        List<Session> sessions = cinema.find(session -> true);
-//        assertThat(sessions, is(Arrays.asList(new Session3D())));
-//    }
-//
-//    @Test
-//    public void add() {
-//        Cinema cinema = new Cinema3D();
-//        Session sessionInit = new Session3D();
-//        cinema.add(sessionInit);
-//        assertThat(sessionInit, is(cinema.find(session -> true).get(0)));
-//    }
+
+    @Ignore
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+    @Ignore
+    @Test
+    public void whenAdd() {
+        Cinema cinema = new Cinema3D();
+        Session sessionInit = new Session3D();
+        cinema.add(sessionInit);
+        assertThat(sessionInit, is(cinema.find(session -> true).get(0)));
+    }
+
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 30, 23, 00);
+        Ticket ticket = cinema.buy(account, 111, 222, date);
+    }
+
+    @Ignore
+    @Test (expected = IllegalArgumentException.class)
+    public void whenInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 13, 30, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = Exception.class)
+    public void whenTwoTicketsInOnePlace() {
+        Account account1 = new AccountCinema();
+        Account account2 = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 9, 30, 23, 00);
+        Ticket ticket1 = cinema.buy(account1, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account2, 1, 1, date);
+    }
 }
