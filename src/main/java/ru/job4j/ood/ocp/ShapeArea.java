0b3527c1fd5c6d847c1b4dd032adec2dcdc8b201
ProstoARS <arsudakov@gmail.com>
2022-05-10 22:35:25 +0300

    0. Принцип открытости закрытости [#4914 #298074]

--- src/main/java/ru/job4j/ood/ocp/ShapeArea.java
+++ src/main/java/ru/job4j/ood/ocp/ShapeArea.java
@@ -2,14 +2,27 @@ package ru.job4j.ood.ocp;
 
 
 public class ShapeArea {
+    public double area(Rectangle rect) {
+        return rect.getHeight() * rect.getWeight();
+    }
+
+    public double area(Circle circle) {
+        return Math.pow(circle.getRadius(), 2) * Math.PI;
+    }
+
+    public Circle areaIncrease(Circle circle, double a) {
+        double radius = Math.sqrt((a * area(circle)) / Math.PI);
+        System.out.println(radius);
+        return new Circle(radius);
+    }
 
     public double areaSum(Object... shapes) {
         double sum = 0;
         for (Object obg : shapes) {
             if (obg.getClass() == Rectangle.class) {
-                sum += ((Rectangle) obg).getHeight() * ((Rectangle) obg).getWeight();
+                sum += area((Rectangle) obg);
             } else {
-                sum += ((Circle) obg).getRadius() * Math.PI;
+                sum += area((Circle) obg);
             }
         }
         return sum;
@@ -35,13 +48,13 @@ class Rectangle {
 }
 
 class Circle {
-    private final int radius;
+    private final double radius;
 
-    public Circle(int radius) {
+    public Circle(double radius) {
         this.radius = radius;
     }
 
-    public int getRadius() {
+    public double getRadius() {
         return radius;
     }
 }
